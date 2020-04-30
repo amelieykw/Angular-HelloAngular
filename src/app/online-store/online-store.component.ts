c9e56a98f11eb949bf1b818311094ca40ad1f78b
amelieykw <yu.kaiwen.amelie@gmail.com>
2020-04-30 18:21:58 +0800

    feat: practice form verification and validation

--- src/app/online-store/online-store.component.ts
+++ src/app/online-store/online-store.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Model } from './repository.model';
 import { Product } from './product.model';
 
@@ -33,6 +34,7 @@ export class OnlineStoreComponent implements OnInit {
     return product.name === this.selectedProduct;
   }
 
+  // add new Product
   newProduct: Product = new Product();
   get jsonProduct() {
     return JSON.stringify(this.newProduct);
@@ -42,4 +44,37 @@ export class OnlineStoreComponent implements OnInit {
     console.log("New Product: " + this.jsonProduct);
   }
 
+  // verify form optimization - errors
+  getValidationMessage(state: any, thingName?: string) {
+    let hostOfError: string = state.path || thingName;
+    let message: string[] = [];
+    if (state.errors) {
+      for (let errorName in state.errors) {
+        switch (errorName) {
+          case 'required':
+            message.push(`You must enter a ${hostOfError}.`);
+            break;
+          case 'minlength':
+            message.push(`A ${hostOfError} must be at least ${state.errors['minlength'].requiredLength}.`);
+            break;
+          case 'pattern':
+            message.push(`the ${hostOfError} contains illegal characters.`);
+            break;
+        }
+      }
+    }
+    return message;
+  }
+
+  // verify form by NgForm
+  formSubmitted: boolean = false;
+  submitForm(form: NgForm) {
+    this.formSubmitted = true;
+    if (form.valid) {
+      this.addProduct(this.newProduct);
+      this.newProduct = new Product();
+      form.reset();
+      this.formSubmitted = false;
+    }
+  }
 }
